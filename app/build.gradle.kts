7b1bd4a94fde5d659b5640d43cb1c3cb57f4e117
William Guilherme Dahlke <williamgdahlke@gmail.com>
2025-01-29 21:23:25 -0300

    version 1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 28
         targetSdk = 35
         versionCode = 1
-        versionName = "1.4"
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
